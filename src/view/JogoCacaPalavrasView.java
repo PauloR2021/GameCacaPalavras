7e9c2a17036c234e108fd267cdd7461b08e659be
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-07-28 18:51:27 -0300

    Add:Adicionando a Classe que extende do JFrame para mostrar ao Usuário

---
+++ src/view/JogoCacaPalavrasView.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JogoCacaPalavrasView extends JFrame {
+    public JogoCacaPalavrasView() {
+        setTitle("Caça-Palavras de Jogadores de Basquete");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+    }
+}
